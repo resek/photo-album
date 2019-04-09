d07c1983e79b6d870d7bae4d193a7062ab8a3c1b
resek <matija.resek@gmail.com>
2019-04-09 11:38:55 +0200

    photos backgrounds & photo spinner

--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -1,6 +1,6 @@
 import React from "react";
 import CircularProgress from '@material-ui/core/CircularProgress';
 
-const Spinner = () => <CircularProgress size={80} style={{marginTop: 100}}/>
+const Spinner = () => <CircularProgress size={80} style={{marginTop: 180}}/>
 
 export default Spinner;
--- src/containers/Photo/Photo.js
+++ src/containers/Photo/Photo.js
@@ -7,7 +7,7 @@ import { Link } from "react-router-dom";
 class Photo extends Component {
 
     state = {
-        overlay: false,
+        overlay: true,
     }
 
     componentDidMount() {
@@ -32,15 +32,13 @@ class Photo extends Component {
         
         if(this.props.Store.photos.length && this.props.Store.users.length && this.props.Store.albums.length) {
 
-            const userId  = this.props.match.params.id;
-            const albumId = this.props.match.params.albumId;
-            const photoId = this.props.match.params.photoId;
+            const {id, albumId, photoId}  = this.props.match.params;
 
             const album = this.props.Store.albums.filter(album =>
                 album.id === Number(albumId));
 
             const user = this.props.Store.users.filter(user =>
-                user.id === Number(userId));
+                user.id === Number(id));
 
             const photos = this.props.Store.photos.filter(photo => 
                 photo.id === Number(photoId));        
@@ -49,7 +47,7 @@ class Photo extends Component {
                 
                 <div onClick={this.toggleOverlay} key={photo.id} className={classes.Photo}>    
                     
-                    <img src={photo.url} alt=""/>
+                    <img src={photo.url} alt="" />
                     
                     {this.state.overlay ? 
                         <div className={classes.Overlay}>
@@ -57,7 +55,7 @@ class Photo extends Component {
                             <p>Album: {album[0].title}</p>
                             <div>
                                 <p>User: {user[0].name}</p>
-                                <Link className={classes.Link} to={`/${userId}/albums/${albumId}`}><button>See album</button></Link>
+                                <Link className={classes.Link} to={`/${id}/albums/${albumId}`}><button>See album</button></Link>
                             </div>                            
                         </div> 
                     : null}
